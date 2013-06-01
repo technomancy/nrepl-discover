cb1bca985c07ece0e2d72c87c2fc72eb36966b0f
Phil Hagelberg <technomancy@gmail.com>
2013-05-31 20:03:06 -0700

    Don't fall back to old clojure version.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject nrepl-discover "0.0.0"
   :description "nREPL auto-discovery proof-of-concept."
   :dependencies [[org.clojure/tools.nrepl "0.2.3"]
-                 [org.clojure/tools.trace "0.7.5"]]
+                 [org.clojure/tools.trace "0.7.5"]
+                 [org.clojure/clojure "1.5.1"]]
   :repl-options {:nrepl-middleware [nrepl.discover/wrap-discover]})
