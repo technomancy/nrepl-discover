8727c8d11e6b4d22d882d77865657e0aff4cbdd6
Phil Hagelberg <technomancy@gmail.com>
2013-05-31 20:45:58 -0700

    Add support for running a single test.

--- src/nrepl/discover.clj
+++ src/nrepl/discover.clj
@@ -30,27 +30,24 @@
                    :doc "Toggle tracing of a given var."}}
   nrepl-op
   [{:keys [transport var] :as msg}]
-  (require 'clojure.tools.nrepl.transport 'clojure.tools.nrepl.misc)
-  (let [send (resolve 'clojure.tools.nrepl.transport/send)
-        response-for (resolve 'clojure.tools.nrepl.misc/response-for)]
-    (try
-      (if-let [v (resolve (symbol var))]
-        (if (-> v meta :clojure.tools.trace/traced)
-          (do (trace/untrace-var* v)
-              (send transport (response-for msg :status :done
-                                            :message (str var " untraced."))))
-          (do (trace/trace-var* v)
-              (send transport (response-for msg :status :done
-                                            :message (str var " traced.")))))
-        (send transport (response-for msg :status #{:error :done}
-                                      :message "no such var")))
-      (catch Exception e
-        (#'trace/tracer "Error" (.getMessage e))
-        (send transport (response-for msg :status #{:error :done}
-                                      :message (.getMessage e)))))))
+  (try
+    (if-let [v (resolve (symbol var))]
+      (if (-> v meta :clojure.tools.trace/traced)
+        (do (trace/untrace-var* v)
+            (t/send transport (m/response-for msg :status :done
+                                              :message (str var " untraced."))))
+        (do (trace/trace-var* v)
+            (t/send transport (m/response-for msg :status :done
+                                              :message (str var " traced.")))))
+      (t/send transport (m/response-for msg :status #{:error :done}
+                                        :message "no such var")))
+    (catch Exception e
+      (#'trace/tracer "Error" (.getMessage e))
+      (t/send transport (m/response-for msg :status #{:error :done}
+                                        :message (.getMessage e))))))
 
 ;; example nrepl op for clojure.test
-(defn report [old-report event]
+(defn- report [old-report event]
   (when-let [current-test (last test/*testing-vars*)]
     (alter-meta! current-test
                  assoc :status (conj (:status (meta current-test))
@@ -60,34 +57,71 @@
                                       ((test/file-position 2) 1)])))
   (old-report event))
 
-(defn test-results [namespace]
+(defn- test-results [namespace]
   (filter :status (map meta (vals (ns-interns (symbol namespace))))))
 
-(defn overlay-for [{:keys [status line]}]
+(defn- overlay-for [{:keys [status line]}]
   (cond (some (comp #{:fail} first) status) ["red" line ""]
         (some (comp #{:error} first) status) ["orange" line ""]
         :else ["green" line ""]))
+
+(defn summary-response [msg ns summary results]
+  (let [message (if (pos? (+ (:error summary) (:fail summary)))
+                  "Failed" "Passed")]
+    (doseq [r results]
+      (t/send (:transport msg) (m/response-for msg :overlay (overlay-for r))))
+    (t/send (:transport msg) (m/response-for msg :status :done
+                                             :message message))))
 
 (defn ^{:nrepl/op {:name "run-tests"
                    :doc "Run tests for a namespace"}}
   run-tests
   [{:keys [transport ns] :as msg}]
-  (let [send (resolve 'clojure.tools.nrepl.transport/send)
-        response-for (resolve 'clojure.tools.nrepl.misc/response-for)]
-    (try
-      (send transport (response-for msg :clear-overlay "true"))
-      (let [summary (with-redefs [test/report (partial report test/report)]
-                      (test/run-tests (symbol ns)))
-            message (if (pos? (+ (:error summary) (:fail summary)))
-                      "Failed" "Passed")]
-        (doseq [r (test-results ns)]
-          (send transport (response-for msg :overlay (overlay-for r))))
-        (send transport (response-for msg :status :done
-                                      :message message)))
-      (catch Exception e
-        (#'trace/tracer "Error" (.getMessage e))
-        (send transport (response-for msg :status #{:error :done}
-                                      :message (.getMessage e)))))))
+  (try
+    (t/send transport (m/response-for msg :clear-overlays "true"))
+    (let [summary (with-redefs [test/report (partial report test/report)]
+                    (test/run-tests (symbol ns)))]
+      (summary-response msg ns summary (test-results ns)))
+    (catch Exception e
+      (t/send transport (m/response-for msg :status #{:error :done}
+                                        :message (.getMessage e))))))
 
-;; TODO: run a single test with fixtures
+(defn test-one-var [test-ns test-name]
+  (let [v (ns-resolve (symbol test-ns) (symbol test-name))
+        m (meta (:ns (meta v)))
+        once-fixture-fn (test/join-fixtures (:clojure.test/once-fixtures m))
+        each-fixture-fn (test/join-fixtures (:clojure.test/each-fixtures m))]
+    (once-fixture-fn
+     (fn []
+       (when (:test (meta v))
+         (each-fixture-fn (fn [] (test/test-var v))))))))
+
+(defn test-one [ns test-name]
+  (binding [test/*report-counters* (ref test/*initial-report-counters*)]
+    (let [ns-obj (the-ns (symbol ns))]
+      (test/do-report {:type :begin-test-ns, :ns ns-obj})
+      ;; If the namespace has a test-ns-hook function, call that:
+      (if-let [v (find-var (symbol (str (ns-name ns-obj)) "test-ns-hook"))]
+        ((var-get v))
+        ;; Otherwise, just test every var in the namespace.
+        (test-one-var ns test-name))
+      (test/do-report {:type :end-test-ns, :ns ns-obj}))
+    (test/do-report (assoc @test/*report-counters* :type :summary))
+    @test/*report-counters*))
+
+(defn ^{:nrepl/op {:name "run-test"
+                   ;; TODO: need to get the list of deftests at runtime; eep
+                   :args [["test" "var" "Test: "]]
+                   :doc "Run a single test."}}
+  run-test
+  [{:keys [transport test ns] :as msg}]
+  (try
+    (t/send transport (m/response-for msg :clear-overlays "true"))
+    (let [summary (with-redefs [test/report (partial report test/report)]
+                    (test-one ns test))]
+      (summary-response msg ns summary [(meta (resolve (symbol test)))]))
+    (catch Exception e
+      (t/send transport (m/response-for msg :status #{:error :done}
+                                        :message (.getMessage e))))))
+
 ;; TODO: next/prev failure? (might need hard-coding)
