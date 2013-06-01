b933cf8a3771b53d40d2925a28ba819f17533642
Phil Hagelberg <technomancy@gmail.com>
2013-05-31 20:22:13 -0700

    Add run-tests op for clojure.test support.

--- src/nrepl/discover.clj
+++ src/nrepl/discover.clj
@@ -2,6 +2,7 @@
   (:require [clojure.tools.nrepl.transport :as t]
             [clojure.tools.nrepl.misc :as m]
             [clojure.tools.trace :as trace]
+            [clojure.test :as test]
             [clojure.walk :as w]))
 
 (declare discover)
@@ -47,3 +48,46 @@
         (#'trace/tracer "Error" (.getMessage e))
         (send transport (response-for msg :status #{:error :done}
                                       :message (.getMessage e)))))))
+
+;; example nrepl op for clojure.test
+(defn report [old-report event]
+  (when-let [current-test (last test/*testing-vars*)]
+    (alter-meta! current-test
+                 assoc :status (conj (:status (meta current-test))
+                                     [(:type event) (:message event)
+                                      (str (:expected event))
+                                      (str (:actual event))
+                                      ((test/file-position 2) 1)])))
+  (old-report event))
+
+(defn test-results [namespace]
+  (filter :status (map meta (vals (ns-interns (symbol namespace))))))
+
+(defn overlay-for [{:keys [status line]}]
+  (cond (some (comp #{:fail} first) status) ["red" line ""]
+        (some (comp #{:error} first) status) ["orange" line ""]
+        :else ["green" line ""]))
+
+(defn ^{:nrepl/op {:name "run-tests"
+                   :doc "Run tests for a namespace"}}
+  run-tests
+  [{:keys [transport ns] :as msg}]
+  (let [send (resolve 'clojure.tools.nrepl.transport/send)
+        response-for (resolve 'clojure.tools.nrepl.misc/response-for)]
+    (try
+      (send transport (response-for msg :clear-overlay "true"))
+      (let [summary (with-redefs [test/report (partial report test/report)]
+                      (test/run-tests (symbol ns)))
+            message (if (pos? (+ (:error summary) (:fail summary)))
+                      "Failed" "Passed")]
+        (doseq [r (test-results ns)]
+          (send transport (response-for msg :overlay (overlay-for r))))
+        (send transport (response-for msg :status :done
+                                      :message message)))
+      (catch Exception e
+        (#'trace/tracer "Error" (.getMessage e))
+        (send transport (response-for msg :status #{:error :done}
+                                      :message (.getMessage e)))))))
+
+;; TODO: run a single test with fixtures
+;; TODO: next/prev failure? (might need hard-coding)
